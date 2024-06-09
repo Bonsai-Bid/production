a622ad2310e5103a0b2d2641aa49a2853188206d
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-06-09 14:39:52 -0600

    check tailwind deployment

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,7 +1,7 @@
 import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
-import 'stylesheets/application.css'; // Using Tailwind CSS file
+import './application.css'; // Using Tailwind CSS file
 
 Rails.start();
 Turbolinks.start();
