7abba85940d918f02dedb7b9a0f3f787cedfe2cf
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-06-11 13:52:00 -0600

    add nav bar

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
+
+
+
   protected
 
 
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,6 +3,16 @@ class UserProfile < ApplicationRecord
 
   
   validates :name, presence: true
-  validates :phone, format: { with: /\A\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})\z/, message: "must be a 10 digit number" }, presence: true
+  validates :phone, format: { 
+    with: /\A\+?1?\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})\z/, 
+    message: "must be a valid phone number" 
+  }, presence: true
 
+  before_save :normalize_phone_number
+
+  private
+
+  def normalize_phone_number
+    self.phone = PhonyRails.normalize_number(phone, default_country_code: 'US')
+  end
 end
