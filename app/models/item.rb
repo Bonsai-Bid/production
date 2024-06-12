59d67be0eac174013179a1cd4e6d0f5dd7e5a271
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-06-12 14:56:53 -0600

    fix enums in container

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,18 @@
 class Item < ApplicationRecord
-  enum category_type: { plant: 1, container: 2, essential: 3 }, _prefix: true
-  enum material: { commercial: 1, handmade: 2, disposable: 3, material_other: 4 }, _prefix: true
-  enum shape: { lotus: 1, nanban: 2, round: 3, oval: 4, rectangle: 5, bag: 6, shape_other: 7 }, _prefix: true
-  enum color: { black: 1, white: 2, natural: 3, blue: 4, green: 5, color_other: 6 }, _prefix: true
-  enum origin: { us: 1, japan: 2, china: 3, origin_other: 4 }, _prefix: true
+  enum category_type: { container: 1, essential: 2, plant: 3 }, _prefix: true
+  enum material: { commercial: 1, disposable: 2, handmade: 3, material_other: 4 }, _prefix: true
+  enum shape: { bag: 1, lotus: 2, nanban: 3, oval: 4, rectangle: 5, round: 6, shape_other: 7 }, _prefix: true
+  enum color: { black: 1, blue: 2, green: 3, natural: 4, white: 5, color_other: 6 }, _prefix: true
+  enum origin: { china: 1, japan: 2, us: 3, origin_other: 4 }, _prefix: true
   enum size: { extra_small: 1, small: 2, medium: 3, large: 4, extra_large: 5, extraextra_large: 6, size_other: 7 }, _prefix: true
-  enum essential_type: { soil: 1, supplements: 2, decorations: 3, wire: 4, books: 5, stands: 6, tools: 7, essential_other: 8 }, _prefix: true
-  enum wire_type: { copper: 1, aluminum: 2, wire_other: 3 }, _prefix: true
+  enum essential_type: { books: 1, decorations: 2, soil: 3, stands: 4, supplements: 5, tools: 6, wire: 7, essential_other: 8 }, _prefix: true
+  enum wire_type: { aluminum: 1, copper: 2, wire_other: 3 }, _prefix: true
   enum species: { azalea: 1, boxwood: 2, cypress: 3, elm: 4, ficus: 5, gingko: 6, juniper: 7, maple: 8, oak: 9, olive: 10, pine: 11, schefflera: 12, tea: 13, species_other: 14 }, _prefix: true
-  enum species_category: { deciduous: 1, coniferous: 2, tropical: 3, broadleaf_evergreen: 4, species_category_other: 5 }, _prefix: true
-  enum style: { formal_upright: 1, informal_upright: 2, cascade: 3, forest: 4, clump: 5, broom: 6, style_other: 7 }, _prefix: true
-  enum stage: { pre_bonsai: 1, development: 2, refinement: 3, specimen: 4, seeds_seedlings: 5 }, _prefix: true
-  enum tool_type: { shears: 1, pliers: 2, branch_splitters: 3, cutters: 4, rakes: 5, tool_other: 6 }, _prefix: true
+  enum species_category: { broadleaf_evergreen: 1, coniferous: 2, deciduous: 3, tropical: 4, species_category_other: 5 }, _prefix: true
+  enum style: { broom: 1, cascade: 2, clump: 3, formal_upright: 4, forest: 5, informal_upright: 6, style_other: 7 }, _prefix: true
+  enum stage: { development: 1, pre_bonsai: 2, refinement: 3, seeds_seedlings: 4, specimen: 5 }, _prefix: true
+  enum tool_type: { branch_splitters: 1, cutters: 2, pliers: 3, rakes: 4, shears: 5, tool_other: 6 }, _prefix: true
+  
 
   belongs_to :seller, class_name: 'User'
   has_one :auction, dependent: :destroy
