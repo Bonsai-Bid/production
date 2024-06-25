b733ddc640f8fc8fc6915d1fc0b7dc4574c81377
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-06-24 20:36:55 -0600

    more logging

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,15 @@
 # config/initializers/aws.rb
-Rails.logger.info "AWS Region: #{Rails.application.credentials.dig(:aws, :region)}"
-Rails.logger.info "AWS Access Key ID: #{Rails.application.credentials.dig(:aws, :access_key_id)}"
+region = Rails.application.credentials.dig(:aws, :region)
+access_key_id = Rails.application.credentials.dig(:aws, :access_key_id)
+secret_access_key = Rails.application.credentials.dig(:aws, :secret_access_key)
+
+Rails.logger.info "AWS Region: #{region}"
+Rails.logger.info "AWS Access Key ID: #{access_key_id}"
 
 Aws.config.update({
-  region: Rails.application.credentials.dig(:aws, :region),
+  region: region,
   credentials: Aws::Credentials.new(
-    Rails.application.credentials.dig(:aws, :access_key_id),
-    Rails.application.credentials.dig(:aws, :secret_access_key)
+    access_key_id,
+    secret_access_key
   )
 })
