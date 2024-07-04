08cb7f44b322cd92aaf6e63f425174bd94d31e8c
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-07-03 20:40:42 -0600

    Remove authenticate user where it wasn't needed

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,5 @@
 class AuctionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[edit update destroy]
   before_action :set_auction, only: %i[show edit update destroy]
 
   def show
