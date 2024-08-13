4f9571d32e80dab8ad4e1473ca9b9b9a34dacf9f
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-08-13 16:27:37 -0700

    fix formatting for item and auction info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,22 @@ module ApplicationHelper
     if value.is_a?(Numeric) && value > 0
       number_to_currency(value, unit: '', precision: 2)
     else
-      "No bids yet"
+      "No Bids Yet"
+    end
+  end
+
+  def format_enum(enum_value)
+    def format_enum(enum_value)
+      # Convert the enum value to a string
+      formatted_value = enum_value.to_s
+  
+      # Check if the value is 'us' and capitalize both letters
+      if formatted_value == 'us'
+        formatted_value.upcase
+      else
+        # Default formatting: humanize and titleize
+        formatted_value.humanize.titleize
+      end
     end
   end
 
