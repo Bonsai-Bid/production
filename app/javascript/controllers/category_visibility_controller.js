f37b0b665bf6ddc9762ef0d151a69f5f92a428a2
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-10-02 10:23:38 -0700

    Everything appears as it is supposed to, manually at least

--- app/javascript/controllers/category_visibility_controller.js
+++ app/javascript/controllers/category_visibility_controller.js
@@ -2,7 +2,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["category", "plant", "container", "essential"]
+  static targets = [
+    "category", "plant", "container", "essential", "wire", "tool", "brand", "condition",
+    "material_other", "shape_other", "color_other", "origin_other", "size_other",
+    "essential_other", "wire_other", "tool_other", "condition_other"
+  ]
 
   connect() {
     this.updateVisibility()
@@ -10,58 +14,89 @@ export default class extends Controller {
 
   updateVisibility() {
     const selectedCategory = this.categoryTarget.value
-    console.log("Handling visibility for category:", selectedCategory)
-
     this.hideAllFields()
-    this.showSelectedCategoryFields(selectedCategory)
+
+    switch (selectedCategory) {
+      case 'plant':
+        this.showPlantFields()
+        this.showContainerFields()
+        break
+      case 'container':
+        this.showContainerFields()
+        break
+      case 'essential':
+        this.showEssentialFields()
+        break
+    }
   }
 
   hideAllFields() {
     this.plantTarget.classList.add('hidden')
     this.containerTarget.classList.add('hidden')
     this.essentialTarget.classList.add('hidden')
+    this.wireTarget.classList.add('hidden')
+    this.toolTarget.classList.add('hidden')
+    this.brandTarget.classList.add('hidden')
+    this.conditionTarget.classList.add('hidden')
+    this.material_otherTarget.classList.add('hidden')
+    this.shape_otherTarget.classList.add('hidden')
+    this.color_otherTarget.classList.add('hidden')
+    this.origin_otherTarget.classList.add('hidden')
+    this.size_otherTarget.classList.add('hidden')
+    this.essential_otherTarget.classList.add('hidden')
+    this.wire_otherTarget.classList.add('hidden')
+    this.tool_otherTarget.classList.add('hidden')
+    this.condition_otherTarget.classList.add('hidden')
   }
 
-  showSelectedCategoryFields(category) {
-    switch(category) {
-      case 'plant':
-        this.plantTarget.classList.remove('hidden')
-        this.containerTarget.classList.remove('hidden')
+  showPlantFields() {
+    this.plantTarget.classList.remove('hidden')
+  }
+
+  showContainerFields() {
+    this.containerTarget.classList.remove('hidden')
+  }
+
+  showEssentialFields() {
+    this.essentialTarget.classList.remove('hidden')
+  }
+
+  updateEssentialFields(event) {
+    const selectedEssential = event.target.value
+    this.hideEssentialSubFields()
+
+    switch (selectedEssential) {
+      case 'wire':
+        this.wireTarget.classList.remove('hidden')
         break
-      case 'container':
-        this.containerTarget.classList.remove('hidden')
+      case 'tool':
+        this.toolTarget.classList.remove('hidden')
+        this.brandTarget.classList.remove('hidden')
+        this.conditionTarget.classList.remove('hidden')
         break
-      case 'essential':
-        this.essentialTarget.classList.remove('hidden')
+      case 'other_essential':
+        this.essential_otherTarget.classList.remove('hidden')
         break
     }
   }
 
-  handleCategoryVisibility(selectedCategory) {
-    const categoryFields = {
-      'plant': ['species', 'style', 'stage', 'material', 'shape', 'color', 'origin', 'size'],
-      'container': ['material', 'shape', 'color', 'origin', 'size'],
-      'essential': ['essential_type', 'wire', 'tool', 'brand', 'condition']
-    };
+  hideEssentialSubFields() {
+    this.wireTarget.classList.add('hidden')
+    this.toolTarget.classList.add('hidden')
+    this.brandTarget.classList.add('hidden')
+    this.conditionTarget.classList.add('hidden')
+    this.essential_otherTarget.classList.add('hidden')
+  }
 
-    const allFields = ['plant_fields', 'container_fields', 'essential_fields'];
-    
-    allFields.forEach(fieldId => {
-      const field = document.getElementById(fieldId);
-      if (field) {
-        field.classList.add('hidden');
-      }
-    });
+  updateOtherField(event) {
+    const selectElement = event.target
+    const otherFieldTarget = `${selectElement.name.replace(/[\[\]]/g, '')}_otherTarget`
+    const otherField = this.targets.find(otherFieldTarget)
 
-    if (selectedCategory in categoryFields) {
-      const categoryContainer = document.getElementById(`${selectedCategory}_fields`);
-      if (categoryContainer) {
-        categoryContainer.classList.remove('hidden');
-        
-        categoryContainer.querySelectorAll(':scope > div').forEach(div => {
-          div.classList.remove('hidden');
-        });
-      }
+    if (selectElement.value.includes('other')) {
+      otherField.classList.remove('hidden')
+    } else {
+      otherField.classList.add('hidden')
     }
   }
 }
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   enum color: { black: 1, blue: 2, green: 3, natural: 4, white: 5, color_other: 6 }, _prefix: true
   enum origin: { china: 1, japan: 2, us: 3, origin_other: 4 }, _prefix: true
   enum size: { extra_small: 1, small: 2, medium: 3, large: 4, extra_large: 5, extraextra_large: 6, size_other: 7 }, _prefix: true
-  enum essential_type: { books: 1, decorations: 2, soil: 3, stands: 4, supplements: 5, tools: 6, wire: 7, essential_other: 8 }, _prefix: true
+  enum essential_type: { books: 1, decorations: 2, soil: 3, stands: 4, supplements: 5, tool: 6, wire: 7, essential_other: 8 }, _prefix: true
   enum wire_type: { aluminum: 1, copper: 2, wire_other: 3 }, _prefix: true
   enum species: { azalea: 1, boxwood: 2, cypress: 3, elm: 4, ficus: 5, gingko: 6, juniper: 7, maple: 8, oak: 9, olive: 10, pine: 11, schefflera: 12, tea: 13, species_other: 14 }, _prefix: true
   enum species_category: { broadleaf_evergreen: 1, coniferous: 2, deciduous: 3, tropical: 4, species_category_other: 5 }, _prefix: true
